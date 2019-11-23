c2996d74093c67cbc55154594b37922038a09011
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-23 11:23:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else if (katzDeliLine.length !== 0) {
-    return "Currently serving " + katzDeliLine[1];
+    return "Currently serving " + katzDeliLine[0];
     katzDeliLine.shift();
   }
 }
