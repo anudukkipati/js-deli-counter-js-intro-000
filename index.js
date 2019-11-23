73b3271b11cbd59f1aeec3a5a1da4616a006bf77
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-23 12:05:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function nowServing(katzDeliLine) {
 
   }
 }
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else if (katzDeliLine.length !== 0){
+     var newArray = [];
+     for (var i = 0; i < katzDeliLine.length; i++) {
+    newArray.push(`${i + 1}. ${katzDeliLine[i]}`);
+  }
+  return "The line is currently: " + newArray.join(", ");
+  }
+}
